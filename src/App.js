ccff439919fdb7b9e2db8388bf6a9974d2e900ce
ManishaBairwa9 <manisha.bairwa024@gmail.com>
2024-09-13 19:06:35 +0530

    signup page ui and routing update

--- src/App.js
+++ src/App.js
@@ -23,40 +23,35 @@ const App = () => {
 
     return (
         <Router>
-            <Routes>
+        <Routes>
             <Route path="/" element={
-                <div className='min-h-screen p-2 md:p-14 lg:p-20 pt-28 bg-custom-pattern bg-center flex flex-col justify-center items-center'>
-                <div className='absolute w-[45px] h-[49px] bg-logo bg-cover top-8 left-28'></div>
-                {/* <div className='absolute w-[25rem] h-[20rem] rounded-full bg-[#63E400] opacity-10 top- left-[7rem]'></div> */}
-                {isRegistered && <h1 className='text-xl sm:text-2xl md:text-3xl text-[#001752] font-medium w-full sm:w-4/5 md:w-3/4 lg:w-[40rem] mx-auto text-center py-4 font-outfit z-50'>Encrypted. Secured. Unbreakable. Your Passwords, Always Safe</h1>}
-                {!isRegistered && <h1 className='text-xl sm:text-2xl md:text-3xl text-[#001752] font-medium w-full sm:w-4/5 md:w-3/4 lg:w-[40rem] mx-auto text-center py-4 font-outfit z-50'>Encrypted. Secured. Unbreakable. Your Passwords, Always Safe</h1>}
-                 <div className='mx-auto w-fit flex flex-col md:flex-row gap-10 justify-center items-center mt-14 z-50'>
-                    
-                 {!token ? (
-                     <>
-                         {!isRegistered ? (
-                                        <Register />
-                                    ) : (
-                                        <Login setToken={setToken} />
-                                    )}
-                     </>
-
-                 ) : (
-                     <>
-                         <Credentials token={token} />
-                     </>
-                 )}  
-             </div>
-             
-                <button onClick={handleSuccessfulRegistration} className='font-bold text-sky-800 pt-6'>
-                    {isRegistered ? loginText : registerText}
-                </button>
-                  
-             </div>
+                token ? (
+                    // If the user is logged in, show the Credentials component
+                    <Credentials token={token} />
+                ) : (
+                    // If the user is not logged in, show either Register or Login component
+                    <div className='min-h-screen p-2 md:p-14 lg:p-20 bg-custom-pattern bg-center flex flex-col justify-center items-center'>
+                        <div className='absolute w-[45px] h-[49px] bg-logo bg-cover top-8 left-4 md:left-28'></div>
+                        <h1 className='text-xl sm:text-2xl md:text-3xl text-[#001752] font-medium w-full sm:w-4/5 lg:w-[30rem] mx-auto text-center py-4 font-outfit z-50'>
+                            Encrypted. Secured. Unbreakable. Your Passwords, Always Safe
+                        </h1>
+                        <div className='mx-auto w-fit flex flex-col md:flex-row gap-10 justify-center items-center mt-14 z-50'>
+                            {!isRegistered ? (
+                                <Register />
+                            ) : (
+                                <Login setToken={setToken} />
+                            )}
+                        </div>
+                        <button onClick={handleSuccessfulRegistration} className='font-bold text-sky-800 pt-6'>
+                            {isRegistered ? loginText : registerText}
+                        </button>
+                    </div>
+                )
             } />
-            <Route path="/ip" element={<IPAddressManager token={token}/>} />
+            <Route path="/credentials" element={<Credentials token={token} />} />
+            <Route path="/ip" element={<IPAddressManager token={token} />} />
         </Routes>
-        </Router>
+    </Router>
     );
 };
 
